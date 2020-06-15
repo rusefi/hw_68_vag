07dc0a91538b30c9eff5c929a5dbd665fc3c0291
jharvey <github@jaredharvey.com>
2020-06-14 21:17:36 -0400

    Added CPL file and script.

---
+++ export/create__cpl_jlc_from_CPL.bat
@@ -0,0 +1,2 @@
+python jlc_kicad_tools/jlc_fix.py 68_vag_PNP_R0.1
+pause
--- export/prepare_fabricate.bat
+++ export/prepare_fabricate.bat
@@ -1,14 +1,8 @@
 
-set posInputFile=hw72nb-top-pos.csv
-set posOutputFile=0.2/hw72nb-R0.2-cpl.csv
+set posInputFile=0.1/68_vag_PNP-top-pos.csv
+set posOutputFile=0.1/68_vag_PNP-R0.1_JLC_CPL.csv
 set lib_path=../kicad-libraries
-
-set bomInputFile=0.2/hw72nb.csv
-set bomOutputFile=0.2/hw72nb_R0.2-bom.csv
-
-mkdir 0.2
 
 call %lib_path%/Fabrication/jlc_prepare_position.bat %posInputFile% %posOutputFile% %lib_path%
 
-
-call %lib_path%/Fabrication/jlc_prepare_bom.bat      %bomInputFile% %bomOutputFile% %lib_path%
+pause
