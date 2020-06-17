d773995ef294d9a19af2e8941749d5101478931b
rusefi <rusefillc@gmail.com>
2020-06-16 23:29:42 -0400

    ibom

--- export/gen_ibom.bat
+++ export/gen_ibom.bat
@@ -1,7 +1,7 @@
 
-set version=0.2
+set version=0.1
 set dest_dir=export/%version%
 
 set kicad_path=c:\progra~1\KiCad
 set ibom_cmd=%kicad_path%\bin\python.exe InteractiveHtmlBom\InteractiveHtmlBom\generate_interactive_bom.py --no-browser --name-format %%f_%version% --dest-dir %dest_dir%
-%ibom_cmd% ../hw72nb.kicad_pcb
+%ibom_cmd% ../68_vag_PNP.kicad_pcb
