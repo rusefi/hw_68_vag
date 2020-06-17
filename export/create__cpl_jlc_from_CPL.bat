de6a9da328c681b390515d36ebc6e5cdba2bd50f
jharvey <github@jaredharvey.com>
2020-06-16 22:24:27 -0400

    Added CPL file and script.

--- export/create__cpl_jlc_from_CPL.bat
+++ export/create__cpl_jlc_from_CPL.bat
@@ -1,2 +1,2 @@
-python jlc_kicad_tools/jlc_fix.py 68_vag_PNP_R0.1
+python jlc_kicad_tools/KC-POS_to_JLC.py 68_vag_PNP_R0.1
 pause
--- export/prepare_fabricate.bat
+++
@@ -1,8 +0,0 @@
-
-set posInputFile=0.1/68_vag_PNP-top-pos.csv
-set posOutputFile=0.1/68_vag_PNP-R0.1_JLC_CPL.csv
-set lib_path=../kicad-libraries
-
-call %lib_path%/Fabrication/jlc_prepare_position.bat %posInputFile% %posOutputFile% %lib_path%
-
-pause
